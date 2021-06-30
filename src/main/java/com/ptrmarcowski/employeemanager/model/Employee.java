5d2f783b7f4eca35f94d9c061496cbb439a70d80
ptrmarcowski <piotr.marcowski@gmail.com>
2021-06-30 15:07:34 +0200

    Initial commit

---
+++ src/main/java/com/ptrmarcowski/employeemanager/model/Employee.java
@@ -0,0 +1,25 @@
+package com.ptrmarcowski.employeemanager.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageURL;
+    private String employeeCode;
+
+
+
+
+
+}
