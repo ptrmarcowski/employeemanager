9cebde64ea75e059c5acd82be6389715c0895869
ptrmarcowski <piotr.marcowski@gmail.com>
2021-10-01 13:53:51 +0200

    Added UI Model in Angular and Front-End Elements, also CorsFilter to the App in Spring

---
+++ src/main/resources/webapp/employeemanagerapp/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { EmployeeService } from './employee.service';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public employee: Employee;
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+}
---
+++ src/main/resources/webapp/employeemanagerapp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { EmployeeService } from './employee.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
